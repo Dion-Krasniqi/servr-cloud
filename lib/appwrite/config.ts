859a4bfe40c32407677641dffee716530d4137e2
Dion-Krasniqi <dion.krasniqi6@student.uni-pr.edu>
2025-10-27 21:06:15 +0100

    Integrating appwrites api starting off with a normal client session

---
+++ lib/appwrite/config.ts
@@ -0,0 +1,10 @@
+
+export const appwriteConfig = {
+    endpointUrl: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!,
+    projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT!,
+    databaseId: process.env.NEXT_PUBLIC_APPWRITE_DATABASE!,
+    usersId: process.env.NEXT_PUBLIC_APPWRITE_USERS_TABLE!,
+    filesId: process.env.NEXT_PUBLIC_APPWRITE_FILESS_TABLE!,
+    bucketId: process.env.NEXT_PUBLIC_APPWRITE_BUCKET!,
+    secretKey: process.env.NEXT_APPWRITE_KEY!,
+};
