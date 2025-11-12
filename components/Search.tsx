0f7b3b9be1a80fd5be8461c94763efbd2ed117e5
Dion-Krasniqi <dion.krasniqi6@student.uni-pr.edu>
2025-11-12 13:32:56 +0100

    Clicking on results redirects to page with same type

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from 'react'
 import { Input } from './ui/input'
-import { useSearchParams } from 'next/navigation';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { getFiles } from '@/lib/actions/file.actions';
 import { Models } from 'node-appwrite';
 import Thumbnail from './Thumbnail';
@@ -13,9 +13,16 @@ const Search = () => {
   const [open, setOpen] = useState(false);
   const searchParams = useSearchParams();
   const searchQuery = searchParams.get("query") || "";
+  const router = useRouter();
+  const path = usePathname();
 
   useEffect(()=>{
     const fetchFiles = async()=> {
+      if (!query){
+        setResults([]);
+        setOpen(false);
+        router.push(path.replace(searchParams.toString(),''))
+      }
       const files = await getFiles({searchText:query});
       setResults(files?.rows);
       setOpen(true);
@@ -31,6 +38,13 @@ const Search = () => {
     }
   },[searchQuery]);
 
+  const handleClickItem = (file:Models.Document)=> {
+    setOpen(false);
+    setResults([]);
+    router.push(`/${(file.Type === 'document') ? file.Type + 's' : 'media'}?query=${query}`);
+
+  }
+
   
   return (
     <div>
@@ -38,7 +52,8 @@ const Search = () => {
        <Input value={query} placeholder='Search...' onChange={(e)=>setQuery(e.target.value)}/>
        {open && (<ul>
                     {results.length>0 ? (
-                      results.map((file)=>(<li className='flex items-center justify-between' key={file.$id}>
+                      results.map((file)=>(<li className='flex items-center justify-between' key={file.$id}
+                                               onClick={()=>handleClickItem(file)}>
                         <div className='flex cursor-pointer items-center gap-4'>
                           <Thumbnail type={file.Type} extension={file.Extension} url={file.Url} className='size-9 min-w-9'/>
                           <p className='line-clamp-1'>{file.Name}</p>
