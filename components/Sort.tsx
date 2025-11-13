56641130609a8df1534dc3cebab446b996e82d4a
Dion-Krasniqi <dion.krasniqi6@student.uni-pr.edu>
2025-11-13 09:56:26 +0100

    Added file sorting

--- components/Sort.tsx
+++ components/Sort.tsx
@@ -1,8 +1,32 @@
-import React from 'react'
+'use client';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { sortTypes } from "@/constants";
+import { usePathname, useRouter } from "next/navigation"
 
 const Sort = () => {
+  const router = useRouter();
+  const path = usePathname();
+  const handleSort = (value:string)=> {
+    router.push(`${path}?sort=${value}`)
+
+  }
   return (
-    <div>Sort</div>
+    <Select onValueChange={handleSort} defaultValue={sortTypes[0].value}>
+  <SelectTrigger className="w-[180px]">
+    <SelectValue placeholder={sortTypes[0].value} />
+  </SelectTrigger>
+  <SelectContent>
+    {sortTypes.map((sort)=>(<SelectItem value={sort.value} key={sort.label}>{sort.label}</SelectItem>))}
+    
+    
+  </SelectContent>
+</Select>
   )
 }
 
--- constants/index.ts
+++ constants/index.ts
@@ -16,3 +16,30 @@ export const actionsDropDownItems = [
 export const profilePlaceholder = 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png';
 
 export const MAX_FILE_SIZE = 50 * 1024 * 1024
+
+export const sortTypes = [
+  {
+    label: "Date created (newest)",
+    value: "$createdAt-desc",
+  },
+  {
+    label: "Created Date (oldest)",
+    value: "$createdAt-asc",
+  },
+  {
+    label: "Name (A-Z)",
+    value: "Name-asc",
+  },
+  {
+    label: "Name (Z-A)",
+    value: "Name-desc",
+  },
+  {
+    label: "Size (Highest)",
+    value: "Size-desc",
+  },
+  {
+    label: "Size (Lowest)",
+    value: "Size-asc",
+  },
+];
