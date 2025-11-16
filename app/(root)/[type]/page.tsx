c04f5c81abc9627572d9629a8a99b609d7c0a032
Dion-Krasniqi <dion.krasniqi6@student.uni-pr.edu>
2025-11-16 10:01:59 +0100

    If page is [user] loads proper page

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -1,6 +1,8 @@
 import Card from '@/components/Card';
 import Sort from '@/components/Sort';
+import { Button } from '@/components/ui/button';
 import { getFiles, getTotalSpaceUsed } from '@/lib/actions/file.actions';
+import { signOutUser } from '@/lib/actions/user.actions';
 import { getFileTypeParams } from '@/lib/utils';
 import { FileType, SearchParamProps } from '@/types';
 import { SearchParams } from 'next/dist/server/request/search-params'
@@ -11,11 +13,12 @@ const Page = async({ searchParams, params } : SearchParamProps) => {
   const searchText = ((await searchParams)?.query as string) || "";
   const sort = ((await searchParams)?.sort as string) || "";
   const type = ((await params)?.type as string) || "";
-  const typeFilter = type!='documents' ? ['video','audio','image'] as FileType[] : ['document'] as FileType[];
-  const types = getFileTypeParams(type) as FileType[];
-  const [ files, totalSpace ] = await Promise.all([getFiles({types:typeFilter, limit:10}), getTotalSpaceUsed(typeFilter)]);
-  return (
-    <div style={{backgroundColor:'#b7b7b7ff'}}>
+  if (type!='user'){
+    const typeFilter = type!='documents' ? ['video','audio','image'] as FileType[] : ['document'] as FileType[];
+    const types = getFileTypeParams(type) as FileType[];
+    const [ files, totalSpace ] = await Promise.all([getFiles({types:typeFilter, limit:10}), getTotalSpaceUsed(typeFilter)]);
+    return (
+     <div style={{backgroundColor:'#b7b7b7ff'}}>
         <section className='w-full'>
             <h1 className='h1 capitalize'>
                 {type}
@@ -34,8 +37,19 @@ const Page = async({ searchParams, params } : SearchParamProps) => {
             })}
         </section>
 
-    </div>
-  )
+     </div>)}
+  //maybe make this its own, but works fine for now
+  else {
+    return (<div>
+             <p>Account Preferences</p>
+             <p>Setting 1</p>
+             <p>Setting 2</p>
+             <p>Setting 3</p>
+             <form className='self-center' action={async()=>{'use server';await signOutUser();}}>
+                <Button type='submit' className='text-sm'>Sign Out</Button>
+            </form>
+            </div>)
+  }
 }
 
 export default Page
